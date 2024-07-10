f5b3a911c164f35416040ad66cda119e1502d576
Grigory Dubrovin <thefiregms@gmail.com>
2024-07-10 12:00:13 +0900

    tsk13 add input for adding time

--- src/popup.js
+++ src/popup.js
@@ -1,75 +1,79 @@
 function formatTime(totalSeconds) {
-  let hours = Math.floor(totalSeconds / 3600);
-  let minutes = Math.floor((totalSeconds % 3600) / 60);
-  let seconds = Math.floor(totalSeconds % 60);
+	let hours = Math.floor(totalSeconds / 3600);
+	let minutes = Math.floor((totalSeconds % 3600) / 60);
+	let seconds = Math.floor(totalSeconds % 60);
 
-  hours = String(hours).padStart(2, '0');
-  minutes = String(minutes).padStart(2, '0');
-  seconds = String(seconds).padStart(2, '0');
+	hours = String(hours).padStart(2, '0');
+	minutes = String(minutes).padStart(2, '0');
+	seconds = String(seconds).padStart(2, '0');
 
-  return `${hours}:${minutes}:${seconds}`;
+	return `${hours}:${minutes}:${seconds}`;
 }
 
 function clearTimer() {
-  chrome.storage.sync.remove(['startTime', 'totalTime'], () => {
-    if (chrome.runtime.lastError) {
-      console.error(chrome.runtime.lastError);
-    } else {
-      console.log('Specified keys cleared from chrome.storage.sync');
-    }
-  });
+	chrome.storage.sync.remove(['startTime', 'totalTime'], () => {
+		if (chrome.runtime.lastError) {
+			console.error(chrome.runtime.lastError);
+		} else {
+			console.log('Specified keys cleared from chrome.storage.sync');
+		}
+	});
 }
 
 // Вы можете вызвать эту функцию, например, по нажатию кнопки в popup
 document.getElementById('clear-storage').addEventListener('click', () => {
-  clearTimer()
+	clearTimer()
 });
 
 
 function calculateEarnings(totalSeconds) {
-  // Конвертируем время в часах в рубли по тарифу 600 рублей в час
-  let hours = totalSeconds / 3600;
-  let earnings = hours * 600;
-  return earnings.toFixed(2); // Округляем до двух знаков после запятой
+	// Конвертируем время в часах в рубли по тарифу 600 рублей в час
+	let hours = totalSeconds / 3600;
+	let earnings = hours * 600;
+	return earnings.toFixed(2); // Округляем до двух знаков после запятой
 }
 
 function updateTimer() {
-  chrome.storage.sync.get(['startTime', 'totalTime'], function(result) {
-      let startTime = result.startTime;
-      let totalTime = result.totalTime || 0;
+	chrome.storage.sync.get(['startTime', 'totalTime'], function (result) {
+		let startTime = result.startTime;
+		let totalTime = result.totalTime || 0;
 
-      if (startTime) {
-          let currentTime = new Date().getTime();
-          let elapsedTime = (currentTime - startTime) / 1000; // в секундах
-          totalTime += elapsedTime;
-      }
+		if (startTime) {
+			let currentTime = new Date().getTime();
+			let elapsedTime = (currentTime - startTime) / 1000; // в секундах
+			totalTime += elapsedTime;
+		}
 
-      let formattedTime = formatTime(totalTime);
-      let earnings = calculateEarnings(totalTime);
+		let formattedTime = formatTime(totalTime);
+		let earnings = calculateEarnings(totalTime);
 
-      document.querySelector('.block-time-content p').textContent = formattedTime;
-      document.querySelector('.earnings-content p').textContent = `${earnings} рублей`;
-  });
+		document.querySelector('.block-time-content p').textContent = formattedTime;
+		document.querySelector('.earnings-content p').textContent = `${earnings} рублей`;
+	});
 }
 
-function addTime(addTotalTime){
-  chrome.storage.sync.get(['totalTime'], function(result) {
-    let totalTime = result.totalTime;
+function addTime(addTotalTime) {
+	chrome.storage.sync.get(['totalTime'], function (result) {
+		let totalTime = result.totalTime;
 
-    if (totalTime) {
-        console.log(totalTime);
-        addTotalTime *= 60;
-        addTotalTime += result.totalTime;
-        return chrome.storage.sync.set({ totalTime: addTotalTime }); // сохраняем в памяти браузера
+		if (totalTime) {
+			console.log(totalTime);
+			addTotalTime *= 60;
+			addTotalTime += result.totalTime;
+			return chrome.storage.sync.set({ totalTime: addTotalTime }); // сохраняем в памяти браузера
 
-    } else {
-        addTotalTime *= 60;
-        return chrome.storage.sync.set({ totalTime: addTotalTime });
-    }
-  });
+		} else {
+			addTotalTime *= 60;
+			return chrome.storage.sync.set({ totalTime: addTotalTime });
+		}
+	});
 }
 
-
+const saveButton = document.getElementById('saveButton');
+saveButton.addEventListener('click', function () {
+	const inputField = document.getElementById('inputField');
+	addTime(inputField.value);
+});
 
 updateTimer(); // Вызов функции без обертки в DOMContentLoaded
 
